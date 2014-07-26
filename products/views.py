dc9d97e23cc495cd5c92d7ebfab4d3b673eaf22b
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2014-07-26 05:11:06 -0700

    Add basic view and url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+from products.models import TurbografxHucard
 
 # Create your views here.
+def index(request):
+
+    response = "<h1>HuCard Games:</h1>";
+    response += "<ul>";
+
+    for hu in TurbografxHucard.objects.all():
+        response += "<li>" + hu.name + "</li>"
+
+    response += "</ul>";
+
+    return HttpResponse(response)
